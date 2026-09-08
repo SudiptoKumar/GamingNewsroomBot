88bc145ab497faa180c1ab067e6a605484e0b66b
Sudipto Kumar <140225127+SudiptoKumar@users.noreply.github.com>
2026-09-09 04:31:37 +0600

    Delete slate_selector.py

--- slate_selector.py
+++
@@ -1,4 +0,0 @@
-from event_engine import EventEngine
-
-def select_slate(engine: EventEngine, clusters: list[dict], max_posts: int = 20) -> list[dict]:
-    return engine.diversify(clusters, max_posts=max_posts)
